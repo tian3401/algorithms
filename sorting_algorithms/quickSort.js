a0d7692f5acf3878f71815f8bf2e7fede9327ba2
Tian3401 <matt.chen3401@yahoo.com>
2019-12-27 16:24:38 -0800

    delete unnecessary comments

--- sorting_algorithms/quickSort.js
+++ sorting_algorithms/quickSort.js
@@ -3,7 +3,7 @@ function swap(arr, left, right) {
   let temp = arr[left]; 
   arr[left] = arr[right];
   arr[right] = temp;
-  // console.log('new array after swap: ', arr)
+
 }
 
 function partition(arr, left, right) {
@@ -14,11 +14,11 @@ function partition(arr, left, right) {
   while(left < right) {
     while(arr[left] < pivot) {
       left++; 
-      // console.log('left: ',left)
+ 
     }
     while(arr[right] > pivot) {
       right--;
-      // console.log('right: ',right, 'value: ', arr[right])
+   
     }
     // need this check to make sure that still true after while loops
     if(left < right) {
