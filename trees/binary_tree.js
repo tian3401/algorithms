26ce7e3eb83c22489d8f2a745f3b07140e025495
Tian3401 <matt.chen3401@yahoo.com>
2020-03-23 16:13:18 -0700

    fix binary tree template

--- trees/binary_tree.js
+++ trees/binary_tree.js
@@ -1,21 +1,22 @@
 function isBalanced(root) {
   let balanced = true; 
   
+  function getHeight(node) {
+    if(!node) return 0; 
+  
+    let left = getHeight(node.left),
+        right = getHeight(node.right);
+  
+    if(Math.abs(left - right) > 1) {
+      balanced = false; 
+    }
+  
+    return Math.max(left,right) + 1; 
+  }
+
   getHeight(root);
 
   return balanced; 
 
 }
 
-function getHeight(node) {
-  if(!node) return 0; 
-
-  let left = getHeight(node.left),
-      right = getHeight(node.right);
-
-  if(Math.abs(left - right) > 1) {
-    balanced = false; 
-  }
-
-  return Math.max(left,right) + 1; 
-}
