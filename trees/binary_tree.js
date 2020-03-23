3a6eff7c95dfef5a0c3da18690c2a197644202fa
Tian3401 <matt.chen3401@yahoo.com>
2020-03-23 16:14:29 -0700

    include other templates

--- trees/binary_tree.js
+++ trees/binary_tree.js
@@ -1,3 +1,14 @@
+//standard template for binary tree 
+function maxDepth(root) {
+  if(!root) return 0;
+
+  const left = maxDepth(root.left),
+        right = maxDepth(root.right);
+
+  return Math.max(left, right) + 1; 
+}
+
+//standard template for isBalanced binary tree
 function isBalanced(root) {
   let balanced = true; 
   
@@ -18,5 +29,26 @@ function isBalanced(root) {
 
   return balanced; 
 
+}
+
+//alternative template with helper fn outside of main function
+function isBalanced(root) {
+
+  return getHeight(root) != -1; 
+}
+
+function getHeight(node) {
+
+  if(!node) return 0;
+
+  const left = getHeight(node.left),
+        right = getHeight(node.right); 
+
+  if(left == -1 || right == -1) return -1; 
+  if(Math.abs(left - right) > 1) {
+    return -1; 
+  }
+  
+  return Math.max(left, right) + 1; 
 }
 
