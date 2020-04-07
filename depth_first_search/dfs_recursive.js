96934e96486989bcf17494c1957266f5d3b80f66
Tian3401 <matt.chen3401@yahoo.com>
2020-04-06 23:41:12 -0700

    fix dfs template by removing base case

--- depth_first_search/dfs_recursive.js
+++ depth_first_search/dfs_recursive.js
@@ -2,26 +2,18 @@
 function dfsProblem(problem) {
   const result = [];
   dfs(problem, result, [], 0)
-
   return result; 
 };
 
 //Dfs template for combination and permutation problems 
 function dfs(problem, result, temp, index) {
-  //Some base case is met 
-  if(index == problem.length) {
-    //Add to possible combination/permutation to result
-    result.push([...temp])
-    return;
-  }
+  result.push([...temp]) //base case not needed in this case 
 
   //Build combination/permutation 
   for(let i = index; i < problem.length; i++) { // if permutation i = 0
-    //Edge check 
-    if(index == problem.length) break 
     //Pattern: add, recurse, pop 
     temp.push(problem[index]);
-    dfs(problem, result, temp, index + 1);
+    dfs(problem, result, temp, i + 1); //i + 1 becomes new index 
     temp.pop();
   }
 };
@@ -34,7 +26,6 @@ function dfsBinary(problem, result, temp, index) {
       result.push([...temp])
       return;
     }
-  
     //Build combination/permutation 
     temp.push(problem[index])
     dfsBinary(problem, result, temp, index + 1); //With addition
