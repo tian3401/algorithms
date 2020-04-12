4aa9216a157e5153ff04c9c999554cd6577dce25
Tian3401 <matt.chen3401@yahoo.com>
2020-04-11 23:49:33 -0700

    add dijstras algorithm outline and create pq class

---
+++ breadth_first_search/dijkstras_algo.js
@@ -0,0 +1,29 @@
+function shortestPath() {
+
+  //create a visited ds
+
+  //create res table 
+
+  //create adj list 
+
+  //create queue 
+
+
+  //loop through queue 
+
+}
+
+class PriorityQueue {
+  constructor() {
+    this.queue = [];
+  }
+
+  enqueue() {
+
+  }
+
+  dequeue() {
+
+  }
+}
+
